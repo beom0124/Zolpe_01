e7286723b903142985c356a13b4a3a7d7057a227
beom0124 <beom000124@gmail.com>
2021-04-07 18:01:28 +0900

    BAE01

--- app/src/main/java/com/example/zolpe_01/MainActivity.kt
+++ app/src/main/java/com/example/zolpe_01/MainActivity.kt
@@ -8,52 +8,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavi);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()){
-
-                    case R.id.camera:
-                        setFrag(0);
-                        break;
-                    case R.id.home:
-                        setFrag(1);
-                        break;
-                    case R.id.chatbot:
-                        setFrag(2);
-                        break;
-                }
-                return true;
-            }
-        });
-
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-
-        setFrag(0); // 첫화면 설정
-    }
-
-    // 프래그먼트 교체가 일어나는 메서드
-    private void setFrag(int n){
-
-        manager = getSupportFragmentManager();
-        transaction = manager.beginTransaction();
-
-        switch (n){
-            case 0:
-                transaction.replace(R.id.main_frame, fragment1);
-                transaction.commit();
-                break;
-            case 1:
-                transaction.replace(R.id.main_frame, fragment2);
-                transaction.commit();
-                break;
-            case 2:
-                transaction.replace(R.id.main_frame, fragment3);
-                transaction.commit();
-                break;
-        }
     }
 }
